a17dfb80c238ffece5c55b5833de2284cac39a14
Rodolfo Rolon <kelzemrod@gmail.com>
2023-11-17 16:12:30 +0100

    Header alpha

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,26 @@
+import './styles/Header.css'
+
 const Header = () =>{
     return(
-        <div>
-            <div>
+        <div className="Header">
+            <div className="title">
                 <h1>Klz Blog</h1>
             </div>
             
             <div>
 
+            <div className="RightNav">
+                <div className='Lupa'>
+                        <p>#</p>
+                    </div>
+                    <input placeholder="Buscador" />
+                </div>
+
+                    <div className="Burguer">
+                        <p> About me </p>
+                        <p> Portfolio </p>
+                        <p> More </p>
+                    </div>
             </div>
 
         </div>
